10d75a4acfc3fd0f6b5e39a188c0dfb12e8a1af8
Roman Maksimenko <08ax3roman@gmail.com>
2023-07-03 14:30:50 +0300

    Snake on list v0.0

--- MySnake/MySnake/Engine.cpp
+++ MySnake/MySnake/Engine.cpp
@@ -33,10 +33,8 @@ int Apple_X_Pos, Apple_Y_Pos;
 int To_Turn = 0;
 bool Is_Intersect = false,Turn=true;
 ESnake_Direction Direction=ESD_None;
-std::vector<RECT>SNAKE(6);
-std::vector<RECT>PREV_SNAKE(6);
-CList<RECT> _SNAKE;
-CList<RECT> _Prev_SNAKE;
+CList<RECT> _SNAKE,_SNAKE_TAIL;
+CList<RECT> _Prev_SNAKE, _Prev_SNAKE_TAIL;
 
 
 void Redraw_Snake();
@@ -79,13 +77,6 @@ void Init(HWND hWnd) {
     Apple_Brush = CreateSolidBrush(RGB(255, 255, 255));
     
     Hwnd = hWnd;
-    /*Snake_Len =(int)SNAKE.size()-1;
-    SNAKE[Snake_Len].left = Snake_X_Pos;
-    SNAKE[Snake_Len].top = Snake_Y_Pos;
-    SNAKE[Snake_Len].right = SNAKE[Snake_Len].left + Snake_Size;
-    SNAKE[Snake_Len].bottom = SNAKE[Snake_Len].top + Snake_Size;
-    for (int i = 0; i != Snake_Len; ++i)
-        SNAKE[i] = SNAKE[Snake_Len];*/
     //initilization of Snake
     _Snake_Len = 6;
     _SNAKE = new Node<RECT>;
@@ -101,11 +92,13 @@ void Init(HWND hWnd) {
         tmp->item.right = tmp->item.left + Snake_Size;
         tmp->item.bottom = tmp->item.top + Snake_Size;
         prev_tmp->item = tmp->item;
+        if (i == _Snake_Len - 1) {
+            _SNAKE_TAIL = tmp;
+            _Prev_SNAKE_TAIL = prev_tmp;
+        }
         tmp = tmp->next;
         prev_tmp = prev_tmp->next;
     }
-    tmp->next = nullptr;
-    prev_tmp->next = nullptr;
 
     Redraw_Snake();
     SetTimer(Hwnd, Timer_ID, 100, 0);
@@ -152,43 +145,37 @@ void Check_Snake_Eat_Apple() {
 
     if (Is_Intersect) {
         Redraw_Apple();
-        SNAKE.insert(SNAKE.begin(), PREV_SNAKE[0]);
-        Snake_Len = (int)SNAKE.size() - 1;
-        PREV_SNAKE.resize(PREV_SNAKE.size() + 1);
+        _SNAKE_TAIL->next = new Node<RECT>(_Prev_SNAKE_TAIL->item);
+        _SNAKE_TAIL = _SNAKE_TAIL->next;
         Is_Intersect = false;
     }
     RECT tmp;
-    if (IntersectRect(&tmp, &SNAKE[Snake_Len], &Apple)) {
+   if (IntersectRect(&tmp, &_SNAKE->item, &Apple)) {
         Is_Intersect = true;
     }
 }
 //---------------------------------------------------------------------------------
 void Redraw_Snake() {//redrawing snake
-   // PREV_SNAKE = SNAKE;
     CList<RECT> tmp, prev_tmp;
     tmp = _SNAKE;
     prev_tmp = _Prev_SNAKE;
     while (tmp) {
-        
+        if (tmp->next && !prev_tmp->next) {
+            prev_tmp->next = new Node<RECT>;
+            _Prev_SNAKE_TAIL = prev_tmp->next;
+        }
             prev_tmp->item = tmp->item;
             tmp = tmp->next;
             prev_tmp = prev_tmp->next;
 
     }
     //New head position
-    /*SNAKE[Snake_Len].left = Snake_X_Pos;
-    SNAKE[Snake_Len].top = Snake_Y_Pos;
-    SNAKE[Snake_Len].right = SNAKE[Snake_Len].left + Snake_Size;
-    SNAKE[Snake_Len].bottom = SNAKE[Snake_Len].top + Snake_Size;*/
-
     _SNAKE->item.left = Snake_X_Pos;
     _SNAKE->item.top = Snake_Y_Pos;
     _SNAKE->item.right = _SNAKE->item.left + Snake_Size;
     _SNAKE->item.bottom =_SNAKE->item.top + Snake_Size;
 
     //update snake body coordinates
-  /*  for (int i = 0; i != Snake_Len; ++i)
-        SNAKE[i] = PREV_SNAKE[i+1];*/
     
     tmp = _SNAKE->next;
     prev_tmp = _Prev_SNAKE;
@@ -198,10 +185,8 @@ void Redraw_Snake() {//redrawing snake
         prev_tmp = prev_tmp->next;
     }
     //snake eat`s apple
-    //Check_Snake_Eat_Apple();
+    Check_Snake_Eat_Apple();
     //erase previos snake
-    /*for(int i= Snake_Len;i>=0;--i)
-    InvalidateRect(Hwnd, &PREV_SNAKE[i], FALSE);*/
 
     prev_tmp = _Prev_SNAKE;
     while (prev_tmp) {
@@ -210,8 +195,6 @@ void Redraw_Snake() {//redrawing snake
     }
 
     //draw new snake
-   /* for (int i = Snake_Len; i >= 0; --i)
-    InvalidateRect(Hwnd, &SNAKE[i], FALSE);*/
 
     tmp = _SNAKE;
     while (prev_tmp) {
@@ -226,8 +209,6 @@ void Draw_Snake(HDC hdc, RECT& paint_area) {//Draw a snake
     //Delete previos snake`s frame
     SelectObject(hdc, BG_Pen);
     SelectObject(hdc, BG_Brush);
-    //for(int i= Snake_Len;i>=0;--i)
-    //Rectangle(hdc, PREV_SNAKE[i].left, PREV_SNAKE[i].top, PREV_SNAKE[i].left + Snake_Size, PREV_SNAKE[i].top + Snake_Size);
     Node<RECT>* tmp = _Prev_SNAKE;
     while (tmp) {
         Rectangle(hdc, tmp->item.left, tmp->item.top, tmp->item.left + Snake_Size, tmp->item.top + Snake_Size);
@@ -237,8 +218,6 @@ void Draw_Snake(HDC hdc, RECT& paint_area) {//Draw a snake
     //Drawing snake
     SelectObject(hdc, Snake_Pen);
     SelectObject(hdc, Snake_Brush);
-    //for(int i= Snake_Len;i>=0;--i)
-    //Rectangle(hdc, SNAKE[i].left, SNAKE[i].top, SNAKE[i].left + Snake_Size, SNAKE[i].top + Snake_Size);
     tmp = _SNAKE;
     while (tmp) {
         Rectangle(hdc, tmp->item.left, tmp->item.top, tmp->item.left + Snake_Size, tmp->item.top + Snake_Size);
@@ -261,7 +240,7 @@ void Draw_Apple(HDC hdc, RECT& paint_area) {
 void Draw_Frame(HDC hdc,RECT& paint_area) {//Draw game screen
     Draw_Game_Board(hdc);
     Draw_Snake(hdc, paint_area);
-    //Draw_Apple(hdc, paint_area);
+    Draw_Apple(hdc, paint_area);
 }
 //---------------------------------------------------------------------------------
 int On_Timer() {//Snake`s moving on timer
@@ -285,9 +264,9 @@ int On_Timer() {//Snake`s moving on timer
     default:
         break;
     }
-   /* if (Is_Intersect) {
+   if (Is_Intersect) {
         Redraw_Apple();
-    }*/
+    }
     Redraw_Snake();
     return 0;
 }
